c6fa1a9d0812ed0b51cf0dad20b2bfbf0457171a
thorpermentier <thorpermentier@gmail.com>
2025-11-13 14:38:49 +0100

    demo javaskript 1

---
+++ jsbassis.js
@@ -0,0 +1,19 @@
+let x = 10;
+console.log(x);
+
+let kwadraat = x * x
+console.log(kwadraat)
+
+const tekst = "hello"
+const waarheid = true
+let y = 11
+console.log(x == y)
+let tien = "10"
+
+console.log(x === tien)
+console.log("appel" > "Appel")
+if (x % 2 === 0) console.log("even")
+
+else console.log("oneven")
+
+let a = 3, b = 6
